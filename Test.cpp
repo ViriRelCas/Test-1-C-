73301c28667e5333bf42297235e9199a5d704ef7
Viriato <viriatorelvas@gmail.com>
2025-07-03 07:54:32 +0200

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -5,13 +5,13 @@ using namespace std;
 
 int main(){
     string x;
-    const int screenWidth = 800;
-    const int screenHeight = 450;
     int gold = 0;
     int material = 0;
-    int amount = 1;
-    int upgrade = 0;
-    int upgradecost = 0;
+    int amountgained = 1;
+    int upgrade = 1;
+    int upgradecost = 1;
+    int upgradematcost= 1;
+    int minelevel = 0;
     int energy = 10;
 
     cout << "Welcome to the mining game, type mine to mine some gold! \n";
@@ -29,8 +29,8 @@ int main(){
             {
                 if (chance <= 8) 
                 {
-                    cout << "You mined " << amount << " gold! \n";
-                    gold = gold + amount;
+                    cout << "You mined " << amountgained << " gold! \n";
+                    gold = gold + amountgained;
                     energy = energy - 1;
     
                 } else if (chance == 9){
@@ -39,8 +39,8 @@ int main(){
                     energy = energy -1; 
                 }else if (chance >= 10)
                 {
-                    cout << "You mined extra hard today, you gain " << amount * 2 << " gold! \n";
-                    gold = gold + (amount * 2);
+                    cout << "You mined extra hard today, you gain " << amountgained * 2 << " gold! \n";
+                    gold = gold + (amountgained * 2);
                     energy = energy - 1;
                 }
             } else if (energy == 0)
@@ -50,25 +50,42 @@ int main(){
             
             
             
-        }else if (x == "upgrade")
-        {
-            if (upgrade <= gold){
-                cout << "You upgraded the mine! \n";
-                upgrade = upgrade + 1;
-                upgradecost = upgrade * 2;
-                amount = amount + upgrade;
-
-            } else {
-                cout << "You need more gold! \n";
-            }        
         } else if (x == "rest")
         {
             cout <<"You rest for the day, all energy has been replenished! \n";
             energy = 10;
         } else if (x == "material") {
             cout << "You have " << material << " materials! \n";
+        } else if (x == "upgrade")
+        {
+            if (upgrade <= 9){
+                if (gold <= upgradecost){
+                gold = gold - upgradecost;
+                cout << "You upgraded the mine! \n";
+                upgrade = upgrade + 1;
+                upgradecost = minelevel * 0.5;
+                amountgained = amountgained + minelevel;
+                minelevel = minelevel + 1;
+                } else {
+                    cout << "Not enough gold! \n";
+                }
+
+            }  else if (upgrade == 10){
+                if (material <= upgradematcost){
+                    material = material - upgradematcost;
+                    cout << "You upgraded the mine! \n";
+                    upgrade = 1;
+                    upgradecost = minelevel * 0.5;
+                    amountgained = amountgained + (minelevel * 2);
+                    minelevel = minelevel + 1;
+                } else {
+                    cout << "Not enough materials! \n";
+                }
+            }
+        } else if (x == "level") {
+            cout <<"You mine level is " << minelevel << "\n" ;
         } else {
-            cout <<"Command not found, try again! \n" ;
+            cout <<"Comand not found, try again! \n";
         }
 
     }
